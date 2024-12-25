8b3fc1551a185297fd24a6f131cff3781345e65e
Raihan <raihan8258@gmail.com>
2024-12-26 02:20:35 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 /* 
- Created by Biuni - Gianluca Bonifazi
- Site: www.biuni.it
- Mail: info@biuni.it
- 
- Tag the author if you want to use.
-*/
+ Created by R41H4N 
 
 // Only CSS, no need Javascript.
