ecb443d45ec17845cbd7110ad7b32b606a2c3004
HASIB RAIHAN <raihan8258@gmail.com>
2026-03-23 03:01:32 +0600

    Update run.js

--- api/run.js
+++ api/run.js
@@ -1,13 +1,12 @@
-import fetch from "node-fetch";
-
+// api/dogTest.js
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const data = await response.json();
 
     res.status(200).json({
       success: true,
-      joke: data
+      image: data.message
     });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
